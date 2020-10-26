5180c9cc871de2aebb0ab5206d3f2b74d046d4b3
kangyahya <moch.yahya95@gmail.com>
2020-10-26 23:05:40 +0700

    fixed dummy users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,13 +11,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //
         DB::table('users')->insert([
-            'name' => Str::random(10),
-            'username' => 'admin',
-            'email' => Str::random(10).'@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('admin'),
-            'created_at' => now(),
+          'name' => "Istiharoh",
+          'username' => "master",
+          'email' => "admin@zoho.com",
+          'email_verified_at' => now(),
+          'password' => '$2y$10$plbNRGHWZJKHkpLP1Z8uOuO2fGtEw.epDzmtRhk9QNqy4V.J6gspK' // 123456
         ]);
     }
 }
